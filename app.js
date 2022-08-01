422acf1581dc9fe66180d6909bd91970bba49b36
sfetcu20 <sfetcualexandra19@stud.ase.ro>
2022-08-01 18:14:29 +0300

    api

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
 const express = require("express");
 const app = express();
+const fs = require('fs');
+var bodyParser = require('body-parser')
 
-app.get("/", (req, res) => {
-  // TODO: read all players
+
+var bp = bodyParser.json()
+
+
+app.get("/", bp,(req, res) => {
+  fs.open("db.json","r",(err,data)=>{
+    const js = JSON.parse(data);
+    console.log(js)
+  })
 });
 
-app.post("/", (req, res) => {
-  // TODO: write player to file
+app.post("/", bp,(req, res) => {
+  console.log(req.body);
+  const new_data =JSON.parse(JSON.stringify(req.body));
+
+  if (fs.existsSync("db.json")) {
+      fs.readFile("db.json","utf-8",(err,data)=>{
+                    const old_data = JSON.parse(data);
+                    old_data.push(new_data);
+                  fs.writeFile("db.json",JSON.stringify(old_data))
+              })
+    }
+    
+    else fs.writeFile("db.json",JSON.stringify(file))
+
+  res.status(200).json({message:"ok"})
 });
 
 app.get("/:id", (req, res) => {
